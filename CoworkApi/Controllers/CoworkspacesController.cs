1e5f6be37cc09a20eb3adc8f41dfc89482849d81
Dhruvbhagat <dhruvbhagat1001@gmail.com>
2023-12-11 09:36:50 +1100

    initial api code commit.

---
+++ CoworkApi/Controllers/CoworkspacesController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using CoworkApi.Models;
+using GeoCoordinatePortable;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace CoworkApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CoworkspacesController : ControllerBase
+    {
+        private readonly ILogger<CoworkspacesController> _logger;
+
+        public CoworkspacesController(ILogger<CoworkspacesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<CoworkSpace> Get()
+        {
+            return LoadJson();
+        }
+
+        [Route("getnearestspaces")]
+        [HttpGet]
+        public IEnumerable<CoworkSpace> GetNearestSpaces(double lat, double lng)
+        {
+            return GetNearest(lat, lng);
+        }
+
+        private static List<CoworkSpace> LoadJson()
+        {
+            using (StreamReader r = new StreamReader("coworking-spaces.json"))
+            {
+                return JsonConvert.DeserializeObject<List<CoworkSpace>>(r.ReadToEnd());
+            }
+        }
+
+        private static List<CoworkSpace> GetNearest(double lat, double lng)
+        {
+            using (StreamReader r = new StreamReader("coworking-spaces.json"))
+            {
+                var items = JsonConvert.DeserializeObject<List<CoworkSpace>>(r.ReadToEnd());
+
+                var nearest = new List<CoworkSpace>();
+
+                foreach (var item in items)
+                {
+                    var sCoord = new GeoCoordinate(lat, lng);
+                    var eCoord = new GeoCoordinate(item.latitude, item.longitude);
+
+                    var distance = sCoord.GetDistanceTo(eCoord);
+
+                    if (distance <= 1000)
+                    {
+                        nearest.Add(item);
+                    }
+                }
+
+                //return first instance if no nearest locations found
+                if (nearest.Count == 0)
+                {
+                    return new List<CoworkSpace>() { items[0] };
+                }
+
+                return nearest;
+            }
+        }
+    }
+}
---
+++ CoworkApi/Models/CoworkSpace.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace CoworkApi.Models
+{
+    public class Geopoint
+    {
+        public double lon { get; set; }
+        public double lat { get; set; }
+    }
+
+    public class CoworkSpace
+    {
+        public string organisation { get; set; }
+        public string address { get; set; }
+        public string website { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public Geopoint geopoint { get; set; }
+    }
+}
